5826c7e2af08a6d2a47a6a426b62fba2d11c89ba
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-15 21:30:09 -0300

    feat: aply styles in react native

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,12 @@
-import { Text } from "react-native"
+import { Text, View } from "react-native"
+import { styles } from "./index/styles"
 
 export default function Index() {
-  return <Text>Hello World</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+      <Text style={styles.title}>Hello World</Text>
+      <Text style={styles.title}>Hello World</Text>
+    </View>
+  )
 }
---
+++ src/app/index/styles.ts
@@ -0,0 +1,14 @@
+import { colors } from "@/src/styles/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: colors.green[900],
+    fontSize: 22,
+  },
+})
