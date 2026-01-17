3da3274bf2293ef370edf82443528bc3ec0e472e
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-17 10:47:23 -0300

    feat: implment Modal

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -2,7 +2,7 @@ import { Categories } from "@/components/categories"
 import { Link } from "@/components/link"
 import { colors } from "@/styles/colors"
 import { MaterialIcons } from "@expo/vector-icons"
-import { FlatList, Image, TouchableOpacity, View } from "react-native"
+import { FlatList, Image, Modal, Text, TouchableOpacity, View } from "react-native"
 import { styles } from "./styles"
 
 export default function Main() {
@@ -28,6 +28,22 @@ export default function Main() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+            </View>
+            <Text style={styles.modalLinkName}>
+              Rocketseat
+            </Text>
+            <Text style={styles.modalUrl}>
+              https:www.google.com.br
+            </Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
--- src/app/main/styles.ts
+++ src/app/main/styles.ts
@@ -27,9 +27,40 @@ export const styles = StyleSheet.create({
     borderTopColor: colors.gray[600]
   },
   linksContent:{
-  
     gap:20,
     padding:24,
     paddingBottom:100
+  },
+  modal:{
+    flex:1,
+    justifyContent:"flex-end"
+  },
+  modalContent:{
+    backgroundColor:colors.gray[900],
+    borderTopWidth:1,
+    borderTopColor: colors.gray[800],
+    padding:24,
+    paddingBottom:42,
+  },
+  modalHeader:{
+    width:"100%",
+    flexDirection:"row",
+    alignContent:"center",
+    marginBottom:32
+  },
+  modalCategory:{
+    flex:1,
+    fontSize:16,
+    fontWeight:"500",
+    color:colors.gray[400]
+  },
+  modalLinkName:{
+    color:colors.gray[200],
+    fontSize:18,
+    fontWeight:"600",
+  },
+  modalUrl:{
+    color:colors.gray[400],
+    fontSize:14
   }
 })
