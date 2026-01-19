0db2160ce9fee04809e2e0ea0001df36d280a0be
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-19 20:19:31 -0300

    feat: add first webview

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,29 +1,23 @@
-import { MaterialIcons } from "@expo/vector-icons"
-import { Text, TouchableOpacity, View } from "react-native"
+import { Linking, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-import { Categories } from "@/components/categories"
-import { Input } from "@/components/input"
-import { colors } from "@/styles/colors"
-import { router } from "expo-router"
-import { styles } from "./styles"
-
-export default function Add() {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => { router.back() }}>
-          <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
-        </TouchableOpacity>
-
-        <Text style={styles.title}>Novo</Text>
-      </View>
-      <Text style={styles.label}>Selecione Sua Categoria</Text>
-      <Categories />
-
-      <View style={styles.form}>
-        <Input placeholder="Nome" />
-        <Input placeholder="Url" />
-      </View>
-    </View>
-  )
+    <WebView
+      source={{ uri: 'https://expo.dev/' }}
+      onShouldStartLoadWithRequest={(request) => {
+        if (request.url.includes('accounts.google.com')) {
+          Linking.openURL(request.url);
+          return false;
+        }
+        return true;
+      }}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
